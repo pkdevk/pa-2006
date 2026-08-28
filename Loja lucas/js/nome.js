f59e6d55ab989c0b051c374105da45466665c900
pkdevk <doelitzschedits@gmail.com>
2026-08-27 22:20:04 -0300

    Add files via upload

---
+++ Loja lucas/js/nome.js
@@ -0,0 +1,14 @@
+//variável
+var nome = prompt("Digite seu nome:");
+console.log("Seu nome é: " , nome);
+alert("seu nome é:   "+ nome);
+
+//variável
+var nome = prompt("Digite seu cpf:");
+console.log("Seu Cpf é: " , nome);
+alert("seu cpf é:   "+ nome);
+
+//variável
+var nome = prompt("Digite sua idade:");
+console.log("Sua idade é: " , nome);
+alert("sua idade é:   "+ nome);
