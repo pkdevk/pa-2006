91ff511bf70c3b9b633fa7daeebad0e47d485b6c
pkdevk <doelitzschedits@gmail.com>
2026-09-01 22:21:44 -0300

    Add files via upload

---
+++ skywalker/src/skywalker/Skywalker.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package skywalker;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Skywalker 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Skywalker");
+        System.out.print("João Lindo Saudades");
+        System.out.println(3*3);
+        
+        int Numero1 =10;
+        int Numero2 =20;
+        int resultado;
+        
+        //Processamento
+        // Linhas 30 e 31 soma
+        resultado = Numero1 + Numero2;
+        System.out.println("O resultado da soma é: " + resultado);
+        
+        resultado = Numero1 / Numero2;
+        System.out.println("O resultado da divisão é: " + resultado);
+        
+        resultado = Numero1 * Numero2;
+        System.out.println("O resultado da multiplicação é: " + resultado);
+        
+        resultado = Numero1 - Numero2;
+        
+        //Saida de Valores
+        
+        System.out.println(resultado);
+        
+        // TODO code application logic here
+    }
+    
+}
